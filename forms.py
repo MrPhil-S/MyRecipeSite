87458c406dc35a3488a947997af3d02668ea90ca
MrPhil-S <pschims@gmail.com>
2023-01-15 23:28:22 -0800

    merging branch

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FieldList, FormField
+from wtforms import StringField, TextAreaField
 from flask_wtf.file import FileField, file_allowed
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired, Length
 class add_recipe_form(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(min=2, max=200)])
     url = StringField('URL', validators=[Length(max=500)])
-    instructions = StringField('Instructions', validators=[Length(min=2, max=9000)])
+    instructions = TextAreaField('Instructions', validators=[Length(min=2, max=9000)])
     ingredient = StringField('Ingredient', validators=[Length(min=1, max=100)])
     image = FileField("Image")
     submit = SubmitField("Add Recipe")
@@ -15,7 +15,7 @@ class add_recipe_form(FlaskForm):
 class edit_recipe_form(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(min=1, max=200)])
     url = StringField('URL', validators=[Length(max=500)])
-    instructions = StringField('Instructions', validators=[Length(min=1, max=9000)])
+    instructions = TextAreaField('Instructions', validators=[Length(min=1, max=9000)])
     ingredients = StringField('Ingredients', validators=[Length(min=1, max=100)])
     image = FileField("Image")
     submit = SubmitField("Save Changes")
