90079eafa5412326ddb4ac584ed556da46e6cd76
MrPhil-S <pschims@gmail.com>
2024-04-22 22:15:24 -0700

    edit config for prod DB

--- myrecipes/__init__.py
+++ myrecipes/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'testing - need to replace!'
 
 if socket.gethostname() == 'raspberrypi':
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://phil:pythonproj2@192.168.1.143/MyRecipes'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://phil:password_4_PKS@192.168.1.143:3307/MyRecipes'
 else:
 #    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://phil:pythonproj2@192.168.1.143/MyRecipes_DEV'
      app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://phil:password_4_PKS@192.168.1.143:3307/MyRecipes_DEV'
